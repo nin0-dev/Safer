6b938d3762c473c8b06f5b51087cf481982deef2
nin0dev <75569739+nin0-dev@users.noreply.github.com>
2022-07-11 09:29:17 -0400

    Added device admin and declared permissions

---
+++ app/src/main/java/com/nin0dev/safer/DeviceAdmin.kt
@@ -0,0 +1,17 @@
+package com.nin0dev.lostmode
+
+import android.app.admin.DeviceAdminReceiver
+import android.content.Context
+import android.content.Intent
+
+class DeviceAdmin : DeviceAdminReceiver() {
+
+    override fun onEnabled(context: Context, intent: Intent) {
+
+    }
+
+    override fun onDisabled(context: Context, intent: Intent) {
+
+    }
+
+}
