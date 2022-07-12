bcdcd4c3e75baa1deba3eb2bd78461940c595536
nin0dev <75569739+nin0-dev@users.noreply.github.com>
2022-07-12 13:04:55 -0400

    Added dev options verification

--- app/src/main/java/com/nin0dev/safer/MainActivity.kt
+++ app/src/main/java/com/nin0dev/safer/MainActivity.kt
@@ -5,13 +5,17 @@ import android.content.ComponentName
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
+import android.provider.Settings
 import android.provider.Settings.ACTION_APPLICATION_DEVELOPMENT_SETTINGS
 import android.provider.Settings.ACTION_SYNC_SETTINGS
 import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.widget.Button
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.content.ContextCompat
 import com.google.android.material.color.DynamicColors
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,9 +53,8 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(Intent.ACTION_MAIN)
             intent.component =
                 ComponentName("com.android.settings", "com.android.settings.Settings\$MyDeviceInfoActivity")
-            startActivity(intent)
-            findViewById<CoordinatorLayout>(R.id.devOptions).visibility = GONE
-            findViewById<CoordinatorLayout>(R.id.adbDebugging).visibility = VISIBLE
+            startActivityForResult(intent, 2)
+
         }
         backButton.setOnClickListener {
             findViewById<CoordinatorLayout>(R.id.accounts).visibility = VISIBLE
@@ -67,13 +70,32 @@ class MainActivity : AppCompatActivity() {
         }
         devOptionsButton.setOnClickListener {
             val intent = Intent(ACTION_APPLICATION_DEVELOPMENT_SETTINGS)
-            startActivity(intent)
-            findViewById<CoordinatorLayout>(R.id.adbDebugging).visibility = GONE
-            findViewById<CoordinatorLayout>(R.id.connectToPC).visibility = VISIBLE
+            startActivityForResult(intent, 3)
+
         }
         checkPermsButton.setOnClickListener {
 
         }
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 2) {
+            if(Settings.Global.getInt(contentResolver, Settings.Global.DEVELOPMENT_SETTINGS_ENABLED) == 1)
+            {
+                findViewById<CoordinatorLayout>(R.id.devOptions).visibility = GONE
+                findViewById<CoordinatorLayout>(R.id.adbDebugging).visibility = VISIBLE
+            }
+            else
+            {
+                val contextView = findViewById<CoordinatorLayout>(R.id.devOptions)
+                Snackbar.make(contextView, getString(R.string.devOptionsNotEnabled), Snackbar.LENGTH_LONG).setBackgroundTint(resources.getColor(R.color.colorsnackbarSurface)).setTextColor(resources.getColor(R.color.snackbarText)).show()
+            }
+        }
+        if(requestCode == 3) {
+            findViewById<CoordinatorLayout>(R.id.adbDebugging).visibility = GONE
+            findViewById<CoordinatorLayout>(R.id.connectToPC).visibility = VISIBLE
+        }
     }
 }
